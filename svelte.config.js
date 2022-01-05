74877a4970af2cb2cbb1a98a96e8bf4687fcad0a
Greg Miller <hello@gregmiller.io>
2022-01-05 14:42:49 -0500

    chore: Install dependencies
    Add env.example showing sample setup for CMS

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,6 +9,8 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 	},
+
+	preprocess: [preprocess({})],
 }
 
 export default config
