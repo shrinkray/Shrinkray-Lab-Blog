4824956fc70493c8c5dc303076d1ad2107d6f8f5
Greg Miller <hello@gregmiller.io>
2022-01-05 14:12:48 -0500

    style
    </style>: run Prettier

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +6,8 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+		target: '#svelte',
+	},
+}
 
-export default config;
+export default config
