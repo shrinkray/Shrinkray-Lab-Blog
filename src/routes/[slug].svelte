a1453cc4e89ea58fc4e1a32784c64099669d5812
Greg Miller <hello@gregmiller.io>
2022-01-07 17:50:48 -0500

    feat(pages): Add routes/pages
    * Use code from /posts to setup similar /pages
    * Set routes/[slug].svelte to handle page html content

---
+++ src/routes/[slug].svelte
@@ -0,0 +1,33 @@
+<script context="module">
+  export async function load({ params,  fetch }) {
+		const { slug } = params
+		const res = await fetch(`/pages/${slug}.json`)
+	
+		if (res.ok) {
+			const { page } = await res.json()
+			return {
+				props: { page },
+			}
+		}
+	}
+</script>
+
+
+
+<script>
+  export let page
+
+	
+</script>
+
+<svelte:head>
+  <title>Shrinkray Labs Blog | </title>
+</svelte:head>
+
+
+
+<h1 class='text-4xl font-semibold mb-5'>{page.title}</h1>
+
+<article class='prose'>
+	{@html page.content.html}
+</article>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,25 @@
-<script>
-	import '../app.css'
+<script context="module">
+  export const load = async ({ fetch }) => {
+		const res = await fetch('/pages.json')
+		if (res.ok) {
+			const { pages } = await res.json()
+			return {
+				props: { pages },
+			}
+		}
+	}
 </script>
+
+<script>
+	import Nav from '$lib/Nav.svelte'
+	import '../app.css'
+	
+	export let pages
+	
+</script>
+
+<Nav {pages} />
+
 <main class='container max-w-xl mx-auto px-4'>
 	<slot />
 </main>
